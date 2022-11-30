bb0ed6db82e4c6d93e8ee54f503a50b545820061
w4sabi <jp.figueredo8@gmail.com>
2022-11-30 17:21:49 -0300

    Feature: Adding checkpoint-1

---
+++ dh-front-llI-master-CRUDZAO/src/App.jsx
@@ -0,0 +1,47 @@
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+
+import { InstagramTimeLine } from './challenges/InstagramTimeLine'
+import { QuartaAula } from './lessons/QuartaAula'
+import { QuintaAula } from './lessons/QuintaAula'
+import { TerceiraAula } from './lessons/TerceiraAula'
+import { DHGames } from './pages/DHGames'
+import { SetimaAula } from './lessons/SetimaAula'
+import { DecimaTerceiraAula } from './lessons/DecimaTerceiraAula'
+import { DecimaQuartaAula } from './lessons/DecimaQuartaAula'
+import { DecimaQuintaAula } from './lessons/DecimaQuintaAula'
+import { MainLayout } from "./components/MainLayout"
+import { ToDo } from "./pages/ToDo"
+
+function App() {
+
+  const appRouter = createBrowserRouter([
+    {
+      path: '',
+      element: <MainLayout />,
+      children: [
+        {
+          path: 'decima-quarta-aula',
+          element: <DecimaQuartaAula />
+        },
+        {
+          path: 'decima-quinta-aula',
+          element: <DecimaQuintaAula />
+        }
+      ]
+    },
+    {
+      path: 'to-do',
+      element: <ToDo />
+    }
+  ])
+
+  return (
+    <RouterProvider router={appRouter} />
+  )
+
+}
+
+export default App
--- dh-front-lll-master/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import { InstagramTimeLine } from "./challenges/InstagramTimeLine";
-import { QuintaAula } from "./lessons/QuintaAula";
-
-function App() {
-  return <InstagramTimeLine />;
-}
-
-export default App;
